37785ef8208588e2f9ca933630e182e0bbd6eac6
sulincix <parduscix@yandex.ru>
2024-05-23 16:42:01 +0000

    init

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+mkdir /output -p
+if ! which ympstrap >/dev/null ; then
+    wget https://gitlab.com/turkman/devel/sources/ymp/-/raw/master/scripts/ympstrap -O /bin/ympstrap
+    chmod +x /bin/ympstrap
+fi
+# create rootfs
+if [[ ! -f rootfs/etc/os-release ]] ; then
+    ympstrap rootfs
+fi
+
+# set permissions
+chmod 1777 rootfs/tmp
+chmod 700 rootfs/data/user/root
+chown root:root rootfs/data/user/root
+
+tar --gzip -cfv rootfs/* /output/turkman-rootfs.tar.gz 
