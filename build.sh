d8cb3c6ee85e5d4c90358d00db5a63d9423c6950
sulincix <parduscix@yandex.ru>
2024-05-23 16:51:13 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,6 +16,7 @@ chmod 700 rootfs/data/user/root
 chown root:root rootfs/data/user/root
 mount --bind /proc rootfs/proc
 chroot rootfs ymp clean --allow-oem
+chroot rootfs ymp rbd --allow-oem > /output/rootfs.revdeb-rebuild
 umount -lf rootfs/proc
 
 tar --gzip -cf /output/turkman-rootfs-minimal.tar.gz rootfs/*
